cba81d47ab35777ad7d7261b3a80be39775c8922
ianmarquez1129 <ianmarquez1129@gmail.com>
2023-07-01 23:22:23 +0800

    chore: remove unused viewmodels

--- app/src/main/java/com/zmci/safetymonitoringapp/home/camera/AddCameraViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.zmci.safetymonitoringapp.home.camera
-
-import androidx.lifecycle.ViewModel
-
-class AddCameraViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/zmci/safetymonitoringapp/home/camera/CameraCredentialsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.zmci.safetymonitoringapp.home.camera
-
-import androidx.lifecycle.ViewModel
-
-class CameraCredentialsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
