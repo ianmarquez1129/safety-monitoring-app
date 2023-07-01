752beef42e9981ac2b5e6034454519741f8e1d19
ianmarquez1129 <ianmarquez1129@gmail.com>
2023-07-01 23:21:18 +0800

    chore: add binding to AboutFragment

--- app/src/main/java/com/zmci/safetymonitoringapp/about/AboutFragment.kt
+++ app/src/main/java/com/zmci/safetymonitoringapp/about/AboutFragment.kt
@@ -1,26 +1,23 @@
 package com.zmci.safetymonitoringapp.about
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.zmci.safetymonitoringapp.Backend
-import com.zmci.safetymonitoringapp.R
-import com.zmci.safetymonitoringapp.UserData
+import androidx.fragment.app.Fragment
+import com.zmci.safetymonitoringapp.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+
+    private var _binding : FragmentAboutBinding? = null
+    private val binding by lazy { _binding!! }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    ): View {
+        _binding = FragmentAboutBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +27,7 @@ class AboutFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 
 }
